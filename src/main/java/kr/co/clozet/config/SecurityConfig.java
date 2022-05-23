5db65aa6db6f7e2186859fc4d54b2209d7789c79
seunghyun <suji1541@gmail.com>
2022-05-23 15:01:18 +0900

    2022 05 23 15:00 보안

---
+++ src/main/java/kr/co/clozet/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package kr.co.clozet.config;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+/**
+ * packageName   :   kr.co.clozet.config
+ * fileName      :   SecurityConfig.java
+ * author        :   Kimseunghyun
+ * date          :   2022-05-23
+ * desc          :
+ * ============================================
+ * DATE             AUTHOR              NOTE
+ * ============================================
+ * 2022-05-23       Kimseunghyun         최초 생성
+ */
+public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        super.configure(builder);
+    }
+}
